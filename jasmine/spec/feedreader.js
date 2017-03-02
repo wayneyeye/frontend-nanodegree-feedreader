592cda7aebbd0dc77cee93001d69dfdad4aae8ae
yewenhe0904 <yewenhe0904@gmail.com>
2017-03-01 22:43:22 -0600

    15 init new feed select.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -110,8 +110,9 @@ $(function() {
             done();
          });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
-
+    });
+    /* TODO: Write a new test suite named "New Feed Selection"*/
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
