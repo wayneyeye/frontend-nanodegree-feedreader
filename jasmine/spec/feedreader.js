58d44dafe1e910254d25634947bfa0b61c33cceb
yewenhe0904 <yewenhe0904@gmail.com>
2017-03-01 21:57:57 -0600

    13 add init entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -78,11 +78,11 @@ $(function() {
                 expect($('body').hasClass('menu-hidden')).toBe(true);
             }
          });
-
+    });
 
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
