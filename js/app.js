bce747a2195cb10d1be001c35e33523568f44789
yewenhe0904 <yewenhe0904@gmail.com>
2017-03-01 20:48:42 -0600

    6 set the allFeeds to fail the test

--- js/app.js
+++ js/app.js
@@ -7,21 +7,22 @@
  */
 
 // The names and URLs to all of the feeds we'd like available.
-var allFeeds = [
-    {
-        name: 'Udacity Blog',
-        url: 'http://blog.udacity.com/feed'
-    }, {
-        name: 'CSS Tricks',
-        url: 'http://feeds.feedburner.com/CssTricks'
-    }, {
-        name: 'HTML5 Rocks',
-        url: 'http://feeds.feedburner.com/html5rocks'
-    }, {
-        name: 'Linear Digressions',
-        url: 'http://feeds.feedburner.com/udacity-linear-digressions'
-    }
-];
+// var allFeeds = [
+//     {
+//         name: 'Udacity Blog',
+//         url: 'http://blog.udacity.com/feed'
+//     }, {
+//         name: 'CSS Tricks',
+//         url: 'http://feeds.feedburner.com/CssTricks'
+//     }, {
+//         name: 'HTML5 Rocks',
+//         url: 'http://feeds.feedburner.com/html5rocks'
+//     }, {
+//         name: 'Linear Digressions',
+//         url: 'http://feeds.feedburner.com/udacity-linear-digressions'
+//     }
+// ];
+var allFeeds = [];
 
 /* This function starts up our application. The Google Feed
  * Reader API is loaded asynchonously and will then call this
