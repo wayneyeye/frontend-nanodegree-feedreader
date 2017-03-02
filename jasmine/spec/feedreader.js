043784e7a18701f95c535890361870ac302fc987
yewenhe0904 <yewenhe0904@gmail.com>
2017-03-01 21:19:59 -0600

    8 Feeds urls not null

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('are not empty urls', function(){
+            for (var i=0; i<allFeeds.length;i++){
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url).not.toBe('');
+            }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
