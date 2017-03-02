7db1f6d2dc8063ad52c09890149d816949eaa1eb
yewenhe0904 <yewenhe0904@gmail.com>
2017-03-02 08:38:19 -0600

    fix new feed logic

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -45,34 +45,42 @@ $(function() {
 
     describe('Initial Entries', function() {
          var container;
-
          beforeEach(function(done){
             loadFeed(0,done);
          });
 
-         it('should have at least one single entry element',function(){
+         it('should have at least one single entry element',function(done){
             container=$('.feed .entry');
             expect(container.length>0).toBe(true);
+            done();
          });
 
     });
     describe('New Feed Selection', function() {
-        var container = $('.feed'),
-            originalHTML,newHTML;
-         beforeEach(function(done){
-            if (allFeeds.length<2) throw "only one feed to use!";//error handling for out of bound array access
-            loadFeed(0,function(){ //init a feed
-                done();
-            });
-            originalHTML=container.children()[0];
-            loadFeed(1,function(){ //change a feed
-                done();
-            });
+        var originalHTML=[],newHTML=[];
+        beforeEach(function(done){
+            //error handling for out of bound array access
+            if (allFeeds.length<2) throw "only one feed to use!";
+            //init a feed
+            loadFeed(0,function(){ 
+                $('.feed .entry').each(function(){
+                    originalHTML.push($(this).html());//saving elements into an array
+                });
+                // chaining second loadFeed
+                loadFeed(1,function(){
+                    $('.feed .entry').each(function(){
+                        newHTML.push($(this).html());
+                    });//second loadFeed method
+                    done();
+                });
+            });   
          });
-
-         it('should change the content',function(done){
-            newHTML=container.children()[0];
-            expect(newHTML).not.toBe(originalHTML);//compare old and new
+        //comparing the entries
+        it('should change the content',function(done){
+            console.log(originalHTML.length);
+            for (var i=0;i<Math.min(originalHTML.length,newHTML.length);i++){
+                expect(newHTML[i]).not.toEqual(originalHTML[i]);//compare old and new                    
+            }
             done();
         });         
     });
